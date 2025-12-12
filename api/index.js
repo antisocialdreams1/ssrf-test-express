f99708d0e4d4c4071834835db065be3bac8e6799
antisocialdreams1 <kormuskinaroslav@gmail.com>
2025-12-12 13:54:22 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 module.exports = (req, res) => {
-    console.log('Время запроса:', new Date().toISOString());
-    // Редирект на AWS Metadata Service
-    res.writeHead(302, { 'Location': 'http://169.254.169.254/latest/meta-data/' });
+    // 1. Просто логируем факт запроса
+    console.log('📨 Запрос от сканера? User-Agent:', req.headers['user-agent']);
+    console.log('Время:', new Date().toISOString());
+    console.log('Путь:', req.url);
+    
+    const webhookUrl = 'http://169.254.169.254/latest/meta-data/';
+    
+    console.log('🎯 Редирект на:', webhookUrl);
+    
+    res.writeHead(302, {
+        'Location': webhookUrl,
+        'Cache-Control': 'no-store'
+    });
     res.end();
 };
