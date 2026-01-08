5875381fbe7a0323d1fd6d316ee8875271675719
antisocialdreams1 <kormuskinaroslav@gmail.com>
2026-01-08 13:13:53 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 module.exports = (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('antisocialdreams - server\n' + 
-          'Request received from: ' + req.headers['referer'] + '\n' +
-          'Time: ' + new Date().toISOString());
+    const webhookUrl = 'https://webhook.site/68ce6e00-0e8c-46d9-9efb-a8aa8bd9fffe';
+    
+    console.log('Время запроса:', new Date().toISOString());
+    console.log('Путь:', req.url);
+    console.log('User-Agent:', req.headers['user-agent'] || 'Не указан');
+    
+    res.writeHead(302, {
+        'Location': webhookUrl,
+        'Cache-Control': 'no-store, no-cache, max-age=0'
+    });
+    res.end();
 };
