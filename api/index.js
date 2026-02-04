64ac6a4fc7b0a5e82435daa0286397cb538171da
antisocialdreams1 <kormuskinaroslav@gmail.com>
2026-02-04 18:45:28 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,43 @@
-module.exports = (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('antisocialdreams - server\n' + 
-          'Request received from: ' + req.headers['referer'] + '\n' +
-          'Time: ' + new Date().toISOString());
-};
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>CORS test</title>
+</head>
+
+<body>
+<h1>CORS Test</h1>
+
+<script>
+async function test() {
+    try {
+
+        const response = await fetch(
+            "https://pre-drm.playfamily.ru/widevine/cenc/getlicense",
+            {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: ""
+            }
+        );
+
+        console.log("STATUS:", response.status);
+
+        const text = await response.text();
+
+        console.log("BODY LENGTH:", text.length);
+        console.log("BODY:", text);
+
+    } catch(e) {
+        console.error("FETCH ERROR:", e);
+    }
+}
+
+test();
+</script>
+
+</body>
+</html>
